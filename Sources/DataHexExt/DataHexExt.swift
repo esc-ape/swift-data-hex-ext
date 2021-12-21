e3176a4c2ab61b5488957fac24334535046f73ad
Fengmian Zhou <au0uiw0n5@gmail.com>
2021-12-21 11:53:25 +0000

    Set denotation to true to prefix hex with 0x

--- Sources/DataHexExt/DataHexExt.swift
+++ Sources/DataHexExt/DataHexExt.swift
@@ -2,11 +2,11 @@ import Foundation
 import Array_UInt8HexExt
 
 extension Data {
-  public init(hex: String) {
-    self.init(Array<UInt8>(hex: hex))
-  }
-
-  public func toHexString() -> String {
-    Array(self).toHexString()
-  }
+    public init(hex: String) {
+        self.init(Array<UInt8>(hex: hex))
+    }
+    
+    public func toHexString(_ denotation: Bool) -> String {
+        Array(self).toHexString(denotation)
+    }
 }
